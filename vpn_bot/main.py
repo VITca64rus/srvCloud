22b5fbadcd9a4e9bba063454d7165ef0c8aa257f
vpodlevski <vpodlevski@team.amocrm.com>
2023-07-23 14:42:23 +0300

    celey not found redis

--- vpn_bot/main.py
+++ vpn_bot/main.py
@@ -1,1 +1,62 @@
-print('hello_world')
+import logging
+import os
+
+import markups as nav
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.message import ContentType
+
+logging.basicConfig(level=logging.INFO)
+#Initialize bot
+bot = Bot(token=os.environ.get('TGBOT_TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start' ])
+async def start (message: types.Message):
+    '''Command start'''
+    message_start = f'🖖 Привет {message.from_user.first_name}!\n\n{nav.ABOUT_BOT}'
+    await bot.send_message(message.from_user.id, message_start, reply_markup = nav.mainMenu)
+
+
+@dp.message_handler ()
+async def bot_message(message: types.Message):
+    '''Обработка входящих сообщений'''
+    if message.chat.type == 'private':
+        if message.text == nav.CONNECT_VPN:
+            await bot.send_message(message.from_user.id,
+                                   "После оплаты подписки я пришлю Вам ovpn фаил для доступа через приложение openVPN",
+                                   reply_markup = nav.sub_inline_markup)
+
+
+@dp.callback_query_handler(text="submonth")
+async def submonth(call: types.CallbackQuery):
+    '''Запрос в Юкассу'''
+    await bot.send_invoice(chat_id=call.from_user.id,
+                           title="VPN Нидерданды",
+                           description="Доступ на месяц",
+                           payload="VPN_niderlands_mounth",
+                           provider_token=os.environ.get('YKASSA_TOKEN'),
+                           currency="RUB",
+                           start_parameter="test",
+                           prices=[
+                                    {"label": "Руб",
+                                    "amount": 10000
+                                    }
+                                  ]
+                           )
+
+
+@dp.pre_checkout_query_handler()
+async def process_pre_checkout_query(pre_checkout_query: types.pre_checkout_query):
+    '''Проверка наличия товара'''
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def process_pay(message: types.message):
+    '''Проверка корректности оплаты'''
+    if message.successful_payment.invoice_payload == 'VPN_niderlands_mounth':
+        await message.reply_document(open('/home/vpodlevski/Desktop/test0.ovpn', 'rb'))
+
+
+executor.start_polling(dp, skip_updates = True)
---
+++ vpn_bot/markups.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+#Messages
+ABOUT_BOT = 'Я — srvCloud VPN бот.\n\
+Я подключу вас к VPN за пару простых шагов.\n\
+\n\
+Просто нажмите кнопку "Подключить VPN" и следуйте моим пошаговым инструкциям.'
+
+CONNECT_VPN="🔌 Подключить VPN"
+
+#Main menu
+btnSub = KeyboardButton(CONNECT_VPN)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+mainMenu.add(btnSub)
+
+#Subscribe Inline Buttons
+sub_inline_markup = InlineKeyboardMarkup(row_width=1)
+btnSubMonth = InlineKeyboardMarkup(text="Mecяц - 100 рублей", callback_data="submonth")
+sub_inline_markup.insert(btnSubMonth)
